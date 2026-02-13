8e8bf8e0adb7f292e800e885a8616589b39eab33
FrostCube <nithert25@gmail.com>
2026-02-13 12:27:24 -0500

    Simplify service worker event handlers
    Removed console log from install event and simplified event handlers.

--- sw.js
+++ sw.js
@@ -1,7 +1,2 @@
-self.addEventListener('install', (event) => {
-  console.log('PWA installata correttamente!');
-});
-
-self.addEventListener('fetch', (event) => {
-  // Lascia vuoto per ora, serve solo per il requisito PWA
-});
+self.addEventListener('install', (e) => {});
+self.addEventListener('fetch', (e) => {});
